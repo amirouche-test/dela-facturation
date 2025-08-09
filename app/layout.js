259c2a5def3ca050dd1cc3f8cd9a70f287893b6f
amirouche-test <delmiam24@gmail.com>
2025-08-09 15:45:31 +0100

    new com

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         {children}
         <Toaster position="top-right" />
       </body>
