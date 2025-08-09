fee16ee70a972bdb3588023ae412e5ac6b49d8c9
amirouche-test <delmiam24@gmail.com>
2025-08-09 19:36:01 +0100

    uppdatee

---
+++ app/Head.js
@@ -0,0 +1,11 @@
+// app/head.js
+export default function Head() {
+    return (
+      <>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0d9488" />
+        <link rel="apple-touch-icon" href="/icons/icon-128x128.png" />
+      </>
+    );
+  }
+  
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  });
+  
+  module.exports = withPWA({
+    experimental: {
+      appDir: true, // actif pour Next.js 13+ avec app/
+    },
+    reactStrictMode: true,
+  });
+  
